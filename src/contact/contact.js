e4e514ec905b561c1226674698bedef4686214f1
Antoine Couette <antoine.couette@diginext.fr>
2016-06-14 18:37:04 +0200

    work on contact page

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,52 +1,78 @@
 angular.module('justlogsit')
     .controller('ContactCtrl', ['$translate', '$http', function ($translate, $http) {
-        var vm = this;
-        vm.subject = 'option-1';
-        vm.showErrors = false;
 
         var script = document.createElement('script');
         script.src = 'https://www.google.com/recaptcha/api.js?onload=vcRecaptchaApiLoaded&render=explicit&hl=' + $translate.use();
         document.head.appendChild(script);
 
+        var vm = this;
+        vm.subject = 'option-1';
+        vm.showErrors = false;
+
         vm.submit = function (form) {
-            vm.showErrors = !form.$valid;
+            vm.showErrors = true;
+            vm.submitted = false;
+            vm.errorWhileSending = false;
+
+            console.log('contactCtrl.myRecaptchaResponse : ' + vm.myRecaptchaResponse);
             if (form.$valid) {
-
-
-                $.ajax({
-                    url: "https://formspree.io/a.j.couette@gmail.com",
-                    method: "POST",
-                    data: {message: "hello!"},
-                    dataType: "json"
-                });
-
+                var message = 'Nom : ' + vm.name + '\n' +
+                    'Email : ' + vm.email + '\n' +
+                    'Tél : ' + vm.phone + '\n' +
+                    'Sujet : ' + vm.subject + '\n' +
+                    'Message : ' + vm.message + '\n';
+                var recipient;
+                switch (subject) {
+                    case 'option-1':
+                        recipient = 'a.j.couette@gmail.com';
+                        break;
+                    case 'option-2':
+                        recipient = 'a.j.couette@gmail.com';
+                        break;
+                    case 'option-3':
+                        recipient = 'a.j.couette@gmail.com';
+                        break;
+                }
 
                 $http({
                     method: 'POST',
-                    url: 'https://formspree.io/a.j.couette@gmail.com',
+                    url: 'https://formspree.io/' + recipient,
                     headers: {
                         'Content-Type': 'application/json'
                     },
-                    data: {message: "hello!"}
-                }).then(function successCallback(response) {
-                    // this callback will be called asynchronously
-                    // when the response is available
+                    data: {message: message}
+                }).then(function successCallback() {
+                    vm.submitted = true;
                 }, function errorCallback(response) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
+                    console.log(response);
+                    vm.errorWhileSending = true;
                 });
 
-
-                console.log('contactCtrl.myRecaptchaResponse : ' + vm.myRecaptchaResponse);
-                console.log('name : ' + vm.name);
-                console.log('email : ' + vm.email);
-                console.log('phone : ' + vm.phone);
-                console.log('subject : ' + vm.subject);
-                console.log('message : ' + vm.message);
-
-
-                vm.submitted = true;
 
             }
         };
     }]);
+
+
+translations['en']['contact'] = {
+    "header": "Nous conctacter",
+    "formHeader": "Via le formulaire",
+    "formHeaderMandatoryField": "Champ obligatoire",
+    "formHeaderName": "Nom",
+    "formHeaderEmail": "Adresse email",
+    "formHeaderInvalidEmail": "Adresse email invalide",
+    "formHeaderPhone": "Téléphone",
+    "formHeaderReason1": "Option 1",
+    "formHeaderReason2": "Demande de démo",
+    "formHeaderReason3": "Support technique",
+    "formHeaderMessage": "Entrez votre message ici",
+    "formHeaderMandatoryCaptcha": "La saisie du captcha est obligatoire",
+    "formHeaderSend": "Envoyer",
+    "formHeaderSent": "Votre demande a bien été prise en compte, nous revenons vers vous dès que possible.",
+    "formHeaderErrorWhileSending": "Une erreur est survenue lors de l'envoi de votre message, merci de nous contacter directement par email ou téléphone",
+    "alternativeHeader": "Par email ou téléphone"
+};
+
+translations['fr']['contact'] = {
+    "header": "Nous conctacter",
+};
